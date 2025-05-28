61ead6a27ba1ac50477660ae7def93d5e3bc1068
F.Musharyapov <f.musharyapov@gmail.com>
2025-05-28 17:14:34 +0400

    update Selenoid WebDriver

--- src/test/java/tests/BaseTestSelenoid.java
+++ src/test/java/tests/BaseTestSelenoid.java
@@ -1,18 +1,15 @@
 package tests;
 
-import config.BaseConfig;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.JavascriptExecutor;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
-import org.aeonbits.owner.ConfigFactory;
+import org.testng.annotations.BeforeMethod;
 
 import java.net.URL;
-import java.util.HashMap;
-import java.util.ArrayList;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTestSelenoid {
 
@@ -22,31 +19,29 @@ public class BaseTestSelenoid {
     public void setUp() throws Exception {
         ChromeOptions options = new ChromeOptions();
         options.setCapability("browserVersion", "116.0");
-        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            /* How to add test badge */
-            put("name", "Test badge...");
 
-            /* How to set session timeout */
-            put("sessionTimeout", "15m");
+        // Улучшенная конфигурация для Selenoid
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("name", "Test badge...");
+        selenoidOptions.put("sessionTimeout", "15m");
+        selenoidOptions.put("env", new String[]{"TZ=UTC"});
+        selenoidOptions.put("labels", Map.of("manual", "true"));
+        selenoidOptions.put("enableVNC", true);  // Включение VNC для отладки
+        selenoidOptions.put("enableVideo", false); // Видео можно включить при необходимости
+        selenoidOptions.put("enableLog", true); // Логирование
 
-            /* How to set timezone */
-            put("env", new ArrayList<String>() {{
-                add("TZ=UTC");
-            }});
+        options.setCapability("selenoid:options", selenoidOptions);
 
-            /* How to add "trash" button */
-            put("labels", new HashMap<String, Object>() {{
-                put("manual", "true");
-            }});
-
-            /* How to enable video recording */
-            //put("enableVideo", true);
-        }});
+        // Используем имя сервиса вместо localhost
         this.driver = new RemoteWebDriver(
-                new URL("http://localhost:4445/wd/hub"),
+                new URL("http://selenoid:4444/wd/hub"), // Внутри Docker-сети
                 options
         );
+
+        // Настройка таймаутов
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(20));
     }
 
     @AfterMethod
